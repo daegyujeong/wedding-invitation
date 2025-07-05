127a51c06268e40f4b183c13cd71d3f469078c5f
DaegyuJeong <k5946k@gmail.com>
2025-07-05 18:39:17 +0900

    Create Google Maps widget implementation

---
+++ lib/features/location/presentation/widgets/google_map_widget.dart
@@ -0,0 +1,121 @@
+import 'package:flutter/material.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'dart:async';
+
+class GoogleMapWidget extends StatefulWidget {
+  final double latitude;
+  final double longitude;
+  final String venue;
+  final bool showControls;
+  final double zoom;
+
+  const GoogleMapWidget({
+    super.key,
+    required this.latitude,
+    required this.longitude,
+    required this.venue,
+    this.showControls = true,
+    this.zoom = 16.0,
+  });
+
+  @override
+  State<GoogleMapWidget> createState() => _GoogleMapWidgetState();
+}
+
+class _GoogleMapWidgetState extends State<GoogleMapWidget> {
+  final Completer<GoogleMapController> _controller = Completer();
+  late Set<Marker> _markers;
+  late CameraPosition _initialPosition;
+
+  @override
+  void initState() {
+    super.initState();
+    _initializeMap();
+  }
+
+  void _initializeMap() {
+    _initialPosition = CameraPosition(
+      target: LatLng(widget.latitude, widget.longitude),
+      zoom: widget.zoom,
+    );
+
+    _markers = {
+      Marker(
+        markerId: const MarkerId('venue'),
+        position: LatLng(widget.latitude, widget.longitude),
+        infoWindow: InfoWindow(
+          title: widget.venue,
+          snippet: '결혼식 장소',
+        ),
+        icon: BitmapDescriptor.defaultMarkerWithHue(BitmapDescriptor.hueRose),
+      ),
+    };
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        GoogleMap(
+          mapType: MapType.normal,
+          initialCameraPosition: _initialPosition,
+          markers: _markers,
+          onMapCreated: (GoogleMapController controller) {
+            _controller.complete(controller);
+          },
+          zoomControlsEnabled: widget.showControls,
+          myLocationEnabled: false,
+          myLocationButtonEnabled: false,
+          mapToolbarEnabled: widget.showControls,
+          compassEnabled: widget.showControls,
+          // Disable all gestures for preview mode
+          zoomGesturesEnabled: widget.showControls,
+          scrollGesturesEnabled: widget.showControls,
+          tiltGesturesEnabled: widget.showControls,
+          rotateGesturesEnabled: widget.showControls,
+        ),
+        if (!widget.showControls)
+          Positioned(
+            top: 10,
+            right: 10,
+            child: Container(
+              padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+              decoration: BoxDecoration(
+                color: Colors.white.withOpacity(0.9),
+                borderRadius: BorderRadius.circular(4),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 4,
+                    offset: const Offset(0, 2),
+                  ),
+                ],
+              ),
+              child: const Row(
+                children: [
+                  Icon(
+                    Icons.map,
+                    size: 16,
+                    color: Colors.blue,
+                  ),
+                  SizedBox(width: 4),
+                  Text(
+                    'Google Maps',
+                    style: TextStyle(
+                      fontSize: 12,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
